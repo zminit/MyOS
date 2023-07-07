012963521d7c27895b1609b77b2d37817edab575
zminit <2646863996@qq.com>
2023-07-07 11:40:06 +0800

    creating interruptManger

--- interrupts.h
+++ interrupts.h
@@ -3,12 +3,39 @@
 
 #include "types.h"
 #include "port.h"
+#include "gdt.h"
 
     class InterruptManger
     {
+
+    protected:
+        struct GateDescriptor
+        {
+            uint16_t handlerAddressLowBits;
+            uint16_t gdt_codeSegmentSelector;
+            uint8_t reserved;
+            uint8_t access;
+            uint16_t handleAddressHighBits;
+        } __attribute__((packed));
+
+        static GateDescriptor interruptDescriptorTable[256];
+
+        static void SetInterruptDescriptorTableEntry(
+            uint8_t interruptNumber,
+            uint16_t codeSegmentSelectorOffset,
+            void(*handler)(),
+            uint8_t DescriptorPrivilegeLevel,
+            uint8_t DescriptorType
+        );
+
     public:
+
+        InterruptManger(GlobalDescriptorTable* gdt);
+        ~InterruptManger();
+
         static uint32_t handleInterrupt(uint8_t interruptNumber, uint32_t esp);
 
+        static void IgnoreInterruptRequest();
         static void HandleInterruptRequest0x00();
         static void HandleInterruptRequest0x01();
     };
