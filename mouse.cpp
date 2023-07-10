fad23aa97516f15ab58dbbab86c9997afbeb0711
zminit <2646863996@qq.com>
2023-07-10 18:02:52 +0800

    鼠标实现成功，bug修复

--- mouse.cpp
+++ mouse.cpp
@@ -15,7 +15,7 @@ commandport(0x64)
 
     commandport.Write(0xA8);                        //activate interrupts
     commandport.Write(0x20);                        //get current state
-    uint8_t status = dataport.Read() | 2;
+    uint8_t status = (dataport.Read() | 2) & ~0x20;
     commandport.Write(0x60);                        //set state
     dataport.Write(status);
 
@@ -42,13 +42,16 @@ uint32_t MouseDriver::HandleInterrupt(uint32_t esp)
     if(offset == 0)
     {
         static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+        VideoMemory[80*y+x] = ((VideoMemory[80*y+x] & 0xF000) >> 4)
+                            | ((VideoMemory[80*y+x] & 0x0F00) << 4)
+                            | ((VideoMemory[80*y+x] & 0x00FF));
 
         x += buffer[1];
         if(x < 0) x = 0;
         if(x >= 80) x = 79;
 
-        y += buffer[2];
-        if(y <0) y = 0;
+        y -= buffer[2];
+        if(y < 0) y = 0;
         if(y >= 25)y = 24;
 
         VideoMemory[80*y+x] = ((VideoMemory[80*y+x] & 0xF000) >> 4)
